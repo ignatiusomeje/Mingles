d618c7fb3ab89693c7a25a6907c68fed90b3a54a
Ignatius Omeje <Ignatiusomeje153@gmail.com>
2022-11-03 03:16:44 +0100

    initial commit

--- API/Data/MingleDBContext.cs
+++ API/Data/MingleDBContext.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-
 namespace API.Data;
 
 public class MingleDBContext : IdentityDbContext<AppUser, AppRole, int, IdentityUserClaim<int>, AppUserRole, IdentityUserLogin<int>, IdentityRoleClaim<int>, IdentityUserToken<int>>
@@ -11,4 +10,3 @@ public class MingleDBContext : IdentityDbContext<AppUser, AppRole, int, Identity
       public MingleDBContext(DbContextOptions options) : base(options){
       }
   }
-//   git commit -m "initial commit" -set up framework for identity role and verification class"
